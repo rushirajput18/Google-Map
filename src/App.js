ee564e802e91635ce9b8ff5c7b84fb456fe54ac9
rushirajput18 <rushikeshrajput666@gmail.com>
2023-10-18 21:48:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Initialize the map and heatmap when the component mounts
+  useEffect(() => {
+    // Data points defined as an array of LatLng objects
+    var heatmapData = [
+      new window.google.maps.LatLng(37.782, -122.447),
+      new window.google.maps.LatLng(37.782, -122.445),
+      new window.google.maps.LatLng(37.782, -122.443),
+      new window.google.maps.LatLng(37.782, -122.441),
+      new window.google.maps.LatLng(37.782, -122.439),
+      new window.google.maps.LatLng(37.782, -122.437),
+      new window.google.maps.LatLng(37.782, -122.435),
+      new window.google.maps.LatLng(37.785, -122.447),
+      new window.google.maps.LatLng(37.785, -122.445),
+      new window.google.maps.LatLng(37.785, -122.443),
+      new window.google.maps.LatLng(37.785, -122.441),
+      new window.google.maps.LatLng(37.785, -122.439),
+      new window.google.maps.LatLng(37.785, -122.437),
+      new window.google.maps.LatLng(37.785, -122.435),
+    ];
+
+    var sanFrancisco = new window.google.maps.LatLng(37.774546, -122.433523);
+
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: sanFrancisco,
+      zoom: 13,
+      mapTypeId: 'satellite',
+    });
+
+    var heatmap = new window.google.maps.visualization.HeatmapLayer({
+      data: heatmapData,
+    });
+
+    heatmap.setMap(map);
+  }, []);
+  if (window.google) {
+    // Your Google Maps code here
+  } else {
+    // Handle the case when the Google Maps API is not available
+    console.error('Google Maps API is not available.');
+  }
+  
+  return <div id="map" style={{ height: '500px', width: '100%' }}></div>;
 }
 
 export default App;
